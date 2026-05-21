29ef882f9cc05add180cd51cd84725e520a4b2c6
Marcin <marcin@passbolt.com>
2026-05-21 11:23:47 +0200

    Bump version

--- gradle/versions.gradle.kts
+++ gradle/versions.gradle.kts
@@ -1,5 +1,5 @@
-extra["projectVersionName"] = "3.1.0"
-extra["projectVersionCode"] = 53
+extra["projectVersionName"] = "3.2.0"
+extra["projectVersionCode"] = 54
 
 extra["projectCompileSdk"] = 36
 extra["projectMinSdk"] = 29
