176094b3f9a373dedf4d23a66469d831ba852b8e
mi-mq <marcel.iwanicki@miquido.com>
2026-06-03 12:04:29 +0200

    MOB-4454: Make bottom sheets scrollable

--- features/home/src/main/java/com/passbolt/mobile/android/feature/home/filtersmenu/FiltersMenuBottomSheet.kt
+++ features/home/src/main/java/com/passbolt/mobile/android/feature/home/filtersmenu/FiltersMenuBottomSheet.kt
@@ -4,6 +4,8 @@ import PassboltTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.ModalBottomSheet
@@ -119,6 +121,7 @@ private fun FiltersMenuBottomSheet(
             modifier =
                 Modifier
                     .fillMaxWidth()
+                    .verticalScroll(rememberScrollState())
                     .testTag(FiltersMenu.SHEET),
         ) {
             BottomSheetHeader(
